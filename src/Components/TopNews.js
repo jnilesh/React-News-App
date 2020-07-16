0604c95203ebd2eda2ea2acdaf7afea78ee3ec82
Nilesh Jain <njain.we@gmail.com>
2020-07-16 16:20:29 +0530

    added gnews api back

--- src/Components/TopNews.js
+++ src/Components/TopNews.js
@@ -7,16 +7,17 @@ export default class TopNews extends React.Component {
     
         this.state = {
              articles: null,
+             statusText: "",
 
         };
     }
     
 
     componentDidMount(){
-        // axios.get('https://gnews.io/api/v3/search?q=india&country=in&max=20&image=required&token=031fbd6bd5b5cccf859871f467e9b93e')
-        axios.get('https://newsapi.org/v2/top-headlines?country=in&pageSize=40&apiKey=9552332f84ee4642980836b89205e47d')
+        axios.get('https://gnews.io/api/v3/search?q=india&image=required&token=22367f14c1b088d0e01fa54992382d16')
+        // axios.get('https://newsapi.org/v2/top-headlines?country=in&pageSize=40&apiKey=9552332f84ee4642980836b89205e47d')
             .then(res => {
-                this.setState({ articles : res.data.articles});
+                this.setState({ articles : res.data.articles , statusText: res.statusText});
                 console.log("top news");
                 console.log(this.state.articles);
             })
@@ -30,7 +31,7 @@ export default class TopNews extends React.Component {
 
        
         const articlesDiv = [];
-        if (this.state.articles !== null) {
+        if (this.state.articles !== null && this.state.statusText === 'OK') {
             let len = this.state.articles.length;
             for (let i=0; i < len ; i += 1) {
                 const currentArticle = this.state.articles[i];
@@ -38,13 +39,13 @@ export default class TopNews extends React.Component {
                     <div className="card bg-light border-light rounded shadow-lg mb-3 animate__animated animate__zoomIn" key={i} style={{maxWidth: "100%", }} >
                         <div className="row no-gutters">
                             <div className="col-md-4">
-                                <img src={currentArticle.urlToImage} className="card-img" alt='not available'/>
+                                <img src={currentArticle.image} className="card-img" alt='not available'/>
                             </div>
                             <div className="col-md-8">
                                 <div className="card-body ">
                                     <h5 className="card-title">{currentArticle.title}</h5>
                                     <p className="card-text"><small className="text-muted">{currentArticle.source.name}</small></p>
-                                    <p className="card-text"><a href={currentArticle.url}>{currentArticle.content}</a></p>
+                                    <p className="card-text"><a href={currentArticle.url}>{currentArticle.description}</a></p>
                                     <p className="card-text"><small className="text-muted">{currentArticle.publishedAt}</small></p>
                                 </div>
                             </div>
